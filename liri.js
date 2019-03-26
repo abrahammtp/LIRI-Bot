2157ca22872a198935517e310b6cba1b801e1348
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-03-25 20:30:48 -0400

    The only thing left to do is to finish do-what-it-says, all the other parts of LIRI Bot are working

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@
 
 require("dotenv").config();
 
+var moment = require("moment");
+
 var fs = require("fs");
 
 var axios = require("axios");
@@ -19,15 +21,15 @@ function switches(action) {
         case "concert-this":
             concert();
             break;
-        
+
         case "spotify-this-song":
             song();
             break;
-        
+
         case "movie-this":
             movie();
             break;
-        
+
         case "do-what-it-says":
             doWhat();
             break;
@@ -43,10 +45,15 @@ function concert() {
     var bandsUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
     axios.get(bandsUrl).then(
-        function(response) {
-            console.log("Venue Name: " + response.data[1].venue.name);
-            console.log("Venue Location: " + response.data[1].venue.city + ", " + response.data[1].venue.region);
-            console.log("Date of Event: " + response.data[1].datetime);
+        function (response) {
+            var concerts = response.data;
+            for (let i = 0; i < concerts.length; i++) {
+                console.log("Venue Name: " + concerts[i].venue.name);
+                console.log("Venue Location: " + concerts[i].venue.city);
+                var date = moment(concerts[i].datetime).format("L");
+                console.log("Date of Event: " + date);
+                console.log("----------------------------------------------------");
+            }
         }
     )
 }
@@ -59,13 +66,13 @@ function song() {
     var song = process.argv.slice(3).join(" ");
     spotify
         .search({ type: 'track', query: song, limit: 1 })
-        .then(function(response) {
+        .then(function (response) {
             console.log("Band/Artist: " + response.tracks.items[0].artists[0].name);
             console.log("Song's name: " + response.tracks.items[0].name);
             console.log("Preview link: " + response.tracks.items[0].external_urls.spotify);
-            console.log("Album's name: " + response.tracks.items[0].album.name); 
-    })
-        .catch(function(err) {
+            console.log("Album's name: " + response.tracks.items[0].album.name);
+        })
+        .catch(function (err) {
             console.log(err);
         });
 }
@@ -80,7 +87,7 @@ function movie() {
 
 
     axios.get(moviesUrl).then(
-        function(response) {
+        function (response) {
             console.log("Title: " + response.data.Title);
             console.log("Came out in: " + response.data.Year);
             console.log("IMDB's rating: " + response.data.imdbRating);
@@ -97,22 +104,35 @@ function movie() {
 // read command from txt file
 // parse command from string
 // pass command into switches()
+
+
 function doWhat() {
-    fs.readFile('random.txt', 'UTF-8', function(err, data) {
+    fs.readFile('random.txt', 'UTF-8', function (err, data) {
         if (err) {
             return console.log(err);
         }
         var dataArr = data;
-        var dataArr = data.split(",");
+        dataArr = data.split(" , ");
         console.log(dataArr);
-        var textCommand = dataArr[0]
-        var toSearch = dataArr[1];
+        var textCommand = dataArr[0];
         console.log(textCommand);
-        console.log(toSearch);
-        song = toSearch;
-        movie = toSearch;
-        artist = toSearch;
-        switches(textCommand);
+        
+        // var textCommand = dataArr[0]
+        // console.log(textCommand);
+        // console.log(toSearch);
+        // action = dataArr[0];
+        // switches(action);
     })
 }
 
+// var dataArr2 = dataArr[1].split(" ")
+
+//         console.log(dataArr2)
+//         action = dataArr[0];
+//         value = dataArr2[0]
+//         for (i = 1; i < dataArr2.length; i++) {
+//             value += "+" + dataArr2[i]
+//         }
+//         console.log(value)
+//         console.log(action)
+//         blah()
